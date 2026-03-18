70108a560702093e6b860e926f6b3f1941b60de0
Berlin <berlinc.sss@gmail.com>
2026-03-18 20:37:55 +0000

    Revert "exp: add input skip connection"
    This reverts commit f4d980d1db0bd74a550858b417e6fb5ba28c7cb7.

--- train.py
+++ train.py
@@ -153,11 +153,10 @@ class MLP(nn.Module):
             layers.append(nn.Dropout(dropout))
             dim = hidden_dim
         self.encoder = nn.Sequential(*layers)
-        self.input_skip = nn.Linear(input_dim, dim)
         self.classifier = nn.Linear(dim, num_classes)
 
     def forward(self, inputs):
-        hidden = self.encoder(inputs) + self.input_skip(inputs)
+        hidden = self.encoder(inputs)
         return self.classifier(hidden)
 
 
