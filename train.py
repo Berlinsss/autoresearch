a0b7aac0d42c88e788e3bb6497014ae985e88ba9
Berlin <berlinc.sss@gmail.com>
2026-03-18 20:36:05 +0000

    exp: add layer norm to hidden layers

--- train.py
+++ train.py
@@ -149,6 +149,7 @@ class MLP(nn.Module):
         dim = input_dim
         for _ in range(num_layers):
             layers.append(nn.Linear(dim, hidden_dim))
+            layers.append(nn.LayerNorm(hidden_dim))
             layers.append(build_activation(activation))
             layers.append(nn.Dropout(dropout))
             dim = hidden_dim
